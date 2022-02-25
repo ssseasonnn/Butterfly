db68f11877fad369699e1edf3cfa7947df84e467
ssseasonnn <ssseasonnn@gmail.com>
2022-02-25 16:56:00 +0800

    commit

--- modules/home/src/main/java/zlc/season/home/HomeFragment.kt
+++ modules/home/src/main/java/zlc/season/home/HomeFragment.kt
@@ -5,12 +5,60 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import coil.load
+import kotlinx.coroutines.CoroutineScope
+import zlc.season.home.databinding.FragmentHomeBinding
+import zlc.season.home.databinding.ItemGoodsBinding
+import zlc.season.home.databinding.ItemHeaderBinding
+import zlc.season.home.databinding.ItemTitleBinding
+import zlc.season.yasha.YashaDataSource
+import zlc.season.yasha.YashaItem
+import zlc.season.yasha.grid
+import zlc.season.yasha.linear
 
 class HomeFragment : Fragment() {
+    var binding: FragmentHomeBinding? = null
+
+    val dataSource by lazy { HomeDataSource(lifecycleScope) }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return FragmentHomeBinding.inflate(inflater, container, false).also {
+            this.binding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            recyclerView.grid(dataSource) {
+                spanCount(2)
+
+                renderBindingItem<HeaderItem, ItemHeaderBinding> {
+                    gridSpanSize(2)
+                    viewBinding(ItemHeaderBinding::inflate)
+                    onBind {
+                        itemBinding.ivImg.load(data.img)
+                    }
+                }
+
+                renderBindingItem<TitleItem, ItemTitleBinding> {
+                    gridSpanSize(2)
+                    viewBinding(ItemTitleBinding::inflate)
+                }
+
+                renderBindingItem<GoodsItem, ItemGoodsBinding> {
+                    viewBinding(ItemGoodsBinding::inflate)
+                    onBind {
+                        itemBinding.ivImg.load(data.img)
+                        itemBinding.tvName.text = data.name
+                        itemBinding.tvMoney.text = data.money
+                    }
+                }
+            }
+        }
     }
 }
